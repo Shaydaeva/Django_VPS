f2c8d89abb5cc10720a63994ffbdbe80e9d0ecb8
Shaydaeva <heartless416@gmail.com>
2021-02-09 18:52:56 +0300

    cached_property in basket models

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.utils.functional import cached_property
+
 from mainapp.models import Product
 
 # Способ управления количеством товара при удалении/оформлении корзины
@@ -16,10 +18,14 @@ from mainapp.models import Product
 class Basket(models.Model):
     # objects = BasketQuerySet.as_manager()
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
     add_datetime = models.DateTimeField(verbose_name='время добавления', auto_now_add=True)
+
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
 
     def get_product_cost(self):
         "return cost of all products this type"
@@ -29,19 +35,19 @@ class Basket(models.Model):
 
     def get_total_quantity(self):
         "return total quantity for user"
-        _items = Basket.get_items(user=self.user)
+        _items = self.get_items_cached
         _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
         return _totalquantity
         
-    total_quantity = property(get_total_quantity)
+    # total_quantity = property(get_total_quantity)
 
     def get_total_cost(self):
         "return total cost for user"
-        _items = Basket.get_items(user=self.user)
+        _items = self.get_items_cached
         _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
         return _totalcost
         
-    total_cost = property(get_total_cost)
+    # total_cost = property(get_total_cost)
 
     # def delete(self):
     #     self.product.quantity += self.quantity
