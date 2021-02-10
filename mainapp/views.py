21c22b74b7ad4084c66a3ef0e4d8d6a3cbea5448
Shaydaeva <heartless416@gmail.com>
2021-02-10 21:14:20 +0300

    back

--- mainapp/views.py
+++ mainapp/views.py
@@ -67,8 +67,8 @@ def main(request):
 
 def products(request, pk=None, page=1):   
     title = 'продукты'
-    # links_menu = ProductCategory.objects.filter(is_active=True)
-    links_menu = get_links_menu()
+    links_menu = ProductCategory.objects.filter(is_active=True)
+    # links_menu = get_links_menu()
 
     if pk is not None:
         if pk == 0:
@@ -79,7 +79,8 @@ def products(request, pk=None, page=1):
             # products = Product.objects.filter(is_active=True, category__is_active=True).order_by('price')
             products = Product.objects.filter(is_active=True, category__is_active=True).select_related('category').order_by('price')
         else:
-            category = get_category(pk)
+            category = get_object_or_404(ProductCategory, pk=pk)
+            # category = get_category(pk)
             # products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by('price')
             products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).select_related('category').order_by('price')
 
@@ -115,8 +116,8 @@ def products(request, pk=None, page=1):
     
 def product(request, pk):
     title = 'продукты'
-    # links_menu = ProductCategory.objects.filter(is_active=True)
-    links_menu = get_links_menu()
+    links_menu = ProductCategory.objects.filter(is_active=True)
+    # links_menu = get_links_menu()
 
     product = get_object_or_404(Product, pk=pk)
     
