97f3f6dcfde29340f41851c97f3f19559e197be3
Shaydaeva <heartless416@gmail.com>
2021-02-12 17:30:03 +0300

    test smoke

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.test.client import Client
 
-# Create your tests here.
+from mainapp.models import ProductCategory, Product
+
+
+class TestMainappTestCase(TestCase):
+
+    def setUp(self):
+        category = ProductCategory.objects.create(
+            name='Test1'
+        )
+        Product.objects.create(
+            category=category,
+            name='prod_test_1'
+        )
+        Product.objects.create(
+            category=category,
+            name='prod_test_2'
+        )
+        self.client = Client()
+
+    def test_mainapp_pages(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        
