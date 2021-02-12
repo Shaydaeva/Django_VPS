1ed5694ac6f43ecdf41239f25e157918176605c7
Shaydaeva <heartless416@gmail.com>
2021-02-12 19:09:56 +0300

    test auth user

--- django_project/settings.py
+++ django_project/settings.py
@@ -197,7 +197,7 @@ AUTH_USER_MODEL = 'authapp.ShopUser'
 
 LOGIN_URL = '/auth/login/'
 
-DOMAIN = 'http://localhost:8000'
+DOMAIN_NAME = 'http://localhost:8000'
 
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = '25'
